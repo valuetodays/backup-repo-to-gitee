f42fe76443723a97c23a8ed177d5cec32944edaf
valuetodays <abcdefg20111111@gmail.com>
2025-03-31 20:38:19 +0800

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,12 @@ if [[ "$DEBUG" == "true" ]]; then
 fi
 
 
-#python3 -m venv /py-scripts/env
+python3 -m venv /py-scripts/env
+ls /py-scripts/env/
+
+source /py-scripts/venv/Scripts/activate
 #source /py-scripts/venv/bin/activate
+pip3 install requests==2.25.1
 pip3 install -r /py-scripts/requirements.txt
 
 git lfs install
