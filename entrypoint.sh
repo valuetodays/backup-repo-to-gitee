efce05ec2d7416ddaf9dc57f790e36f44978cfb9
valuetodays <abcdefg20111111@gmail.com>
2025-03-31 20:31:02 +0800

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ if [[ "$DEBUG" == "true" ]]; then
 fi
 
 
-python3 -m venv /py-scripts/env
-source /py-scripts/venv/bin/activate
+#python3 -m venv /py-scripts/env
+#source /py-scripts/venv/bin/activate
 pip3 install -r /py-scripts/requirements.txt
 
 git lfs install
@@ -16,22 +16,21 @@ git lfs install
 echo begin
 echo ${GITHUB_REPOSITORY}
 env
-git init
-git remote add origin https://github.com/${GITHUB_REPOSITORY}.git
-git config http.https://github.com/.extraheader "AUTHORIZATION: basic $(echo -n "x-access-token:${{ inputs.token }}" | base64)"
-git fetch --all
-for branch in `git branch -a | grep remotes | grep -v HEAD`; do
-  git branch --track ${branch##*/} $branch
-done
+# git init
+# git remote add origin https://github.com/${GITHUB_REPOSITORY}.git
+# git config http.https://github.com/.extraheader "AUTHORIZATION: basic $(echo -n "x-access-token:${INPUT_TOKEN}" | base64)"
+# git fetch --all
+# for branch in `git branch -a | grep remotes | grep -v HEAD`; do
+#   git branch --track ${branch##*/} $branch
+# done
 
 _owner=$(echo "${GITHUB_REPOSITORY}"|cut -f1 -d"/")
 _repo_name=$(echo "${GITHUB_REPOSITORY}"|cut -f2 -d"/")
 echo "_owner = ${_owner}, _repo_name = ${_repo_name}"
 
 
-
 python3 /py-scripts/http_utils.py --passcode "${INPUT_PASSWORD}" \
---url 'https://gitee.com/api/v5/user/repos' \
+--url "https://gitee.com/api/v5/user/repos" \
 --reponame "${_repo_name}"
 
 
