638213a74eb73586e92a272126c07a20befc11f6
valuetodays <abcdefg20111111@gmail.com>
2025-03-31 20:59:06 +0800

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,6 @@ fi
 python3 -m venv ~/py_envs
 ls ~/py_envs/bin
 source ~/py_envs/bin/activate
-python3 -m pip install requests==2.25.1
 python3 -m pip install -r /py-scripts/requirements.txt
 
 
@@ -24,13 +23,13 @@ git lfs install
 echo begin
 echo ${GITHUB_REPOSITORY}
 env
-# git init
-# git remote add origin https://github.com/${GITHUB_REPOSITORY}.git
-# git config http.https://github.com/.extraheader "AUTHORIZATION: basic $(echo -n "x-access-token:${INPUT_TOKEN}" | base64)"
-# git fetch --all
-# for branch in `git branch -a | grep remotes | grep -v HEAD`; do
-#   git branch --track ${branch##*/} $branch
-# done
+git init
+git remote add origin https://github.com/${GITHUB_REPOSITORY}.git
+git config http.https://github.com/.extraheader "AUTHORIZATION: basic $(echo -n "x-access-token:${INPUT_TOKEN}" | base64)"
+git fetch --all
+for branch in `git branch -a | grep remotes | grep -v HEAD`; do
+  git branch --track ${branch##*/} $branch
+done
 
 _owner=$(echo "${GITHUB_REPOSITORY}"|cut -f1 -d"/")
 _repo_name=$(echo "${GITHUB_REPOSITORY}"|cut -f2 -d"/")
